ec13ce88c53f3ea06a64f9aec9f1686a8e3d1e3f
qara-qurt <serikov.2002.12@gmail.com>
2023-04-10 19:35:48 +0600

    added swagger docs

--- go.mod
+++ go.mod
@@ -14,6 +14,8 @@ require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
 	github.com/go-openapi/spec v0.20.4 // indirect
@@ -31,6 +33,10 @@ require (
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/swaggo/files/v2 v2.0.0 // indirect
+	github.com/urfave/cli/v2 v2.3.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
--- internal/transport/http/user.go
+++ internal/transport/http/user.go
@@ -6,30 +6,50 @@ import (
 	"net/http"
 	"strconv"
 	"v001_onelab/internal/model"
+
+	_ "v001_onelab/docs"
 )
 
+// GetUsers @Summary GetUsers
+// @Tags users
+// @Security ApiKeyAuth
+// @Description get all users
+// @ID get-users
+// @Accept json
+// @Produce json
+// @Success 200 {object} model.UserResponse
+// @Failure 500 {object} model.ErrorResponse
+// @Router /users/ [get]
 func (h *Handler) GetUsers(c echo.Context) error {
 	res, err := h.UserService.GetAll()
 
 	if err != nil {
 		c.Logger().Error(err)
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(http.StatusInternalServerError, model.NewErrorResponse(err.Error()))
 	}
 
 	return c.JSON(http.StatusOK, res)
 }
 
+// GetUser @Summary GetUser
+// @Tags users
+// @Security ApiKeyAuth
+// @Description get user by ID
+// @ID get-user
+// @Accept json
+// @Produce json
+// @Param  id path int  true "Account ID"
+// @Success 200 {object} model.UserResponse
+// @Failure 500 {object} model.ErrorResponse
+// @Failure 400 {object} model.ErrorResponse
+// @Router /users/{id} [get]
 func (h *Handler) GetUser(c echo.Context) error {
 	paramID := c.Param("id")
 	id, err := strconv.Atoi(paramID)
 
 	if err != nil {
 		c.Logger().Error(err)
-		return c.JSON(http.StatusBadRequest, map[string]string{
-			"error": "invalid user ID",
-		})
+		return c.JSON(http.StatusBadRequest, model.NewErrorResponse("invalid user ID"))
 	}
 
 	res, err := h.UserService.GetByID(id)
@@ -47,20 +67,27 @@ func (h *Handler) GetUser(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
+// SignUp @Summary SignUp
+// @Tags auth
+// @Description register
+// @ID sign-up
+// @Accept json
+// @Produce json
+// @Param  input body model.UserInput  true "User info"
+// @Success 201
+// @Failure 500 {object} model.ErrorResponse
+// @Failure 400 {object} model.ErrorResponse
+// @Router /auth/sign-up [post]
 func (h *Handler) SignUp(c echo.Context) error {
 	var user model.UserInput
 	if err := c.Bind(&user); err != nil {
 		c.Logger().Error(err)
-		return c.JSON(http.StatusBadRequest, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(http.StatusBadRequest, model.NewErrorResponse(err.Error()))
 	}
 
 	if err := user.Validate(); err != nil {
 		c.Logger().Error(err)
-		return c.JSON(http.StatusBadRequest, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(http.StatusBadRequest, model.NewErrorResponse(err.Error()))
 	}
 
 	err := h.UserService.Create(user)
@@ -70,117 +97,134 @@ func (h *Handler) SignUp(c echo.Context) error {
 		if errors.Is(err, model.ErrorAlreadyExist) {
 			status = http.StatusBadRequest
 		}
-		return c.JSON(status, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(status, model.NewErrorResponse(err.Error()))
 	}
-	return c.JSON(http.StatusCreated, map[string]string{
-		"message": "user was created",
-	})
+	return c.NoContent(http.StatusCreated)
 }
 
+// SignIn @Summary SignIn
+// @Tags auth
+// @Description SignIn
+// @ID sign-in
+// @Accept json
+// @Produce json
+// @Param  input body model.SignInInput  true "User info"
+// @Success 200 {object} model.Token
+// @Failure 500 {object} model.ErrorResponse
+// @Failure 400 {object} model.ErrorResponse
+// @Router /auth/sign-in [post]
 func (h *Handler) SignIn(c echo.Context) error {
 	var user model.SignInInput
 	if err := c.Bind(&user); err != nil {
 		c.Logger().Error(err)
-		return c.JSON(http.StatusBadRequest, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(http.StatusBadRequest, model.NewErrorResponse(err.Error()))
 	}
 
 	if err := user.Validate(); err != nil {
 		c.Logger().Error(err)
-		return c.JSON(http.StatusBadRequest, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(http.StatusBadRequest, model.NewErrorResponse(err.Error()))
 	}
 
 	token, err := h.UserService.SignIn(user)
 	if err != nil {
 		c.Logger().Error(err)
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(http.StatusInternalServerError, model.NewErrorResponse(err.Error()))
 	}
 
-	return c.JSON(http.StatusOK, map[string]string{
-		"token": token,
+	return c.JSON(http.StatusOK, model.Token{
+		Token: token,
 	})
 }
 
+// DeleteUser @Summary DeleteUser
+// @Tags users
+// @Security ApiKeyAuth
+// @Description delete user
+// @ID delete-user
+// @Accept json
+// @Produce json
+// @Param  id path int  true "Account ID"
+// @Success 200
+// @Failure 500 {object} model.ErrorResponse
+// @Failure 400 {object} model.ErrorResponse
+// @Router /users/{id} [delete]
 func (h *Handler) DeleteUser(c echo.Context) error {
 	paramID := c.Param("id")
 	id, err := strconv.Atoi(paramID)
 	if err != nil {
 		c.Logger().Error(err)
-		return c.JSON(http.StatusBadRequest, map[string]string{
-			"error": "invalid user ID",
-		})
+		return c.JSON(http.StatusBadRequest, model.NewErrorResponse("invalid user ID"))
 	}
 
 	if err := h.UserService.Delete(id); err != nil {
 		c.Logger().Error(err)
-		return c.JSON(http.StatusBadRequest, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(http.StatusBadRequest, model.NewErrorResponse(err.Error()))
 	}
 
-	return c.JSON(http.StatusOK, map[string]string{
-		"message": "user deleted",
-	})
+	return c.NoContent(http.StatusOK)
 }
 
+// UpdateUser @Summary UpdateUser
+// @Tags users
+// @Security ApiKeyAuth
+// @Description update user
+// @ID update-user
+// @Accept json
+// @Produce json
+// @Param  id path int  true "Account ID"
+// @Success 200
+// @Failure 500 {object} model.ErrorResponse
+// @Failure 400 {object} model.ErrorResponse
+// @Router /users/{id} [patch]
 func (h Handler) UpdateUser(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		c.Logger().Error(err)
-		return c.JSON(http.StatusBadRequest, map[string]interface{}{
-			"error": "invalid user ID",
-		})
+		return c.JSON(http.StatusBadRequest, model.NewErrorResponse("invalid user ID"))
 	}
 
 	var user model.UserResponse
 	if err := c.Bind(&user); err != nil {
 		c.Logger().Error(err)
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(http.StatusInternalServerError, model.NewErrorResponse(err.Error()))
 	}
 
 	if err := user.Validate(); err != nil {
 		c.Logger().Error(err)
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(http.StatusInternalServerError, model.NewErrorResponse(err.Error()))
 	}
 
 	user.ID = uint(id)
 	err = h.UserService.Update(user)
 	if err != nil {
 		c.Logger().Error(err)
-		return c.JSON(http.StatusNotFound, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(http.StatusNotFound, model.NewErrorResponse(err.Error()))
 	}
-	return c.JSON(http.StatusOK, map[string]string{
-		"message": "user updated",
-	})
+	return c.NoContent(http.StatusOK)
 }
 
+// ChangePassword @Summary ChangePassword
+// @Tags users
+// @Security ApiKeyAuth
+// @Description update user
+// @ID change-password
+// @Accept json
+// @Produce json
+// @Param  input body model.ChangePassword  true "info"
+// @Success 200
+// @Failure 500 {object} model.ErrorResponse
+// @Failure 400 {object} model.ErrorResponse
+// @Router /users/change-password/ [patch]
 func (h Handler) ChangePassword(c echo.Context) error {
 	var user model.ChangePassword
 	if err := c.Bind(&user); err != nil {
 		c.Logger().Error(err)
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(http.StatusInternalServerError, model.NewErrorResponse(err.Error()))
 	}
 
 	if err := user.Validate(); err != nil {
 		c.Logger().Error(err)
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(http.StatusInternalServerError, model.NewErrorResponse(err.Error()))
 	}
 
 	if err := h.UserService.ChangePassword(user); err != nil {
@@ -188,33 +232,45 @@ func (h Handler) ChangePassword(c echo.Context) error {
 		if errors.Is(err, model.ErrorPassword) {
 			status = http.StatusBadRequest
 		}
-		return c.JSON(status, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(status, model.NewErrorResponse(err.Error()))
 	}
 
-	return c.JSON(http.StatusOK, map[string]string{
-		"message": "password changed",
-	})
+	return c.NoContent(http.StatusOK)
 }
 
+// GetOrderUserBooks @Summary ChangePassword
+// @Tags user-books
+// @Security ApiKeyAuth
+// @Description get user books current
+// @ID get-user-books
+// @Accept json
+// @Produce json
+// @Success 200 {object} model.UserOrderBooksResponse
+// @Failure 500 {object} model.ErrorResponse
+// @Router /users/books/current [get]
 func (h Handler) GetOrderUserBooks(c echo.Context) error {
 	userBooks, err := h.UserService.GetOrderUserBooks()
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(http.StatusInternalServerError, model.NewErrorResponse(err.Error()))
 	}
 
 	return c.JSON(http.StatusOK, userBooks)
 }
 
+// GetOrderUserBooksLastMounth @Summary ChangePassword
+// @Tags user-books
+// @Security ApiKeyAuth
+// @Description get user books current
+// @ID get-user-books-mounth
+// @Accept json
+// @Produce json
+// @Success 200 {object} model.UserOrderBooksResponse
+// @Failure 500 {object} model.ErrorResponse
+// @Router /users/books/last-mounth [get]
 func (h Handler) GetOrderUserBooksLastMounth(c echo.Context) error {
 	userBooks, err := h.UserService.GetOrderUserBooksLastMounth()
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"error": err.Error(),
-		})
+		return c.JSON(http.StatusInternalServerError, model.NewErrorResponse(err.Error()))
 	}
 
 	return c.JSON(http.StatusOK, userBooks)
